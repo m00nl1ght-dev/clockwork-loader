5dbb346e65f20f0323905894c826dddc9109977a
m00nl1ght <github@m00nl1ght.dev>
2019-10-15 12:54:09 +0200

    impl of plugin info file

--- src/main/java/dev/m00nl1ght/clockwork/locator/ExplodedDirectoryLocator.java
+++ src/main/java/dev/m00nl1ght/clockwork/locator/ExplodedDirectoryLocator.java
@@ -2,11 +2,18 @@ package dev.m00nl1ght.clockwork.locator;
 
 import dev.m00nl1ght.clockwork.api.PluginLocator;
 import dev.m00nl1ght.clockwork.core.PluginDefinition;
+import dev.m00nl1ght.clockwork.util.PluginInfoFile;
+import dev.m00nl1ght.clockwork.util.PluginLoadingException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.File;
+import java.lang.module.ModuleFinder;
 import java.util.function.Consumer;
 
 public class ExplodedDirectoryLocator implements PluginLocator {
+
+    private static final Logger LOGGER = LogManager.getLogger();
 
     private final File lookupPath;
 
@@ -25,7 +32,22 @@ public class ExplodedDirectoryLocator implements PluginLocator {
     }
 
     private void scanDir(File file, Consumer<PluginDefinition> pluginConsumer) {
-        // TODO
+        final var infoFile = new File(file, "META-INF/plugin.toml");
+        if (!file.exists()) return;
+        final var pluginInfo = PluginInfoFile.load(infoFile);
+        final var builder = pluginInfo.populatePluginBuilder();
+        final var moduleFinder = ModuleFinder.of(file.toPath());
+        final var modules = moduleFinder.findAll().iterator();
+        if (!modules.hasNext()) {
+            LOGGER.debug(getName() + " found plugin.toml, but no java module in dir [" + file + "], ignoring");
+            return;
+        }
+
+        builder.moduleFinder(moduleFinder, modules.next().descriptor().name());
+        if (modules.hasNext()) throw PluginLoadingException.generic(getName() + " found multiple java modules in directory [" + file + "]");
+        final var plugin = builder.build();
+        pluginInfo.populateComponents(plugin);
+        pluginConsumer.accept(plugin);
     }
 
     @Override
--- src/main/java/dev/m00nl1ght/clockwork/util/PluginInfoFile.java
+++ src/main/java/dev/m00nl1ght/clockwork/util/PluginInfoFile.java
@@ -2,9 +2,13 @@ package dev.m00nl1ght.clockwork.util;
 
 import com.electronwill.nightconfig.core.UnmodifiableConfig;
 import com.electronwill.nightconfig.core.file.CommentedFileConfig;
+import dev.m00nl1ght.clockwork.core.ComponentDefinition;
+import dev.m00nl1ght.clockwork.core.DependencyDefinition;
 import dev.m00nl1ght.clockwork.core.PluginDefinition;
 
 import java.io.File;
+import java.util.List;
+import java.util.Optional;
 
 public class PluginInfoFile {
 
@@ -22,8 +26,46 @@ public class PluginInfoFile {
         this.config = config;
     }
 
-    public void populatePluginBuilder(PluginDefinition.Builder builder) {
-        // TODO
+    public PluginDefinition.Builder populatePluginBuilder() {
+        final var builder = PluginDefinition.builder(config.get("pluginID"));
+        builder.displayName(config.get("displayName"));
+        builder.version(config.get("version"));
+        builder.description(config.getOrElse("description", ""));
+        builder.authors(config.getOrElse("authors", List.of()));
+        builder.mainClass(config.get("mainClass"));
+        final Optional<List<UnmodifiableConfig>> deps = config.getOptional("dependencies");
+        deps.ifPresent(l -> l.forEach(d -> builder.dependency(buildDep(d))));
+        return builder;
+    }
+
+    public void populateComponents(PluginDefinition plugin) {
+        final Optional<List<UnmodifiableConfig>> components = config.getOptional("components");
+        if (components.isEmpty()) return;
+        for (var conf : components.get()) {
+            final var builder = ComponentDefinition.builder(plugin, conf.get("id"));
+            builder.component(conf.get("class"));
+            builder.target(conf.get("target"));
+            final Optional<List<UnmodifiableConfig>> deps = conf.getOptional("dependencies");
+            deps.ifPresent(l -> l.forEach(d -> builder.dependency(buildDep(d))));
+            final Optional<Boolean> optional = conf.getOptional("optional");
+            optional.ifPresent(builder::optional);
+            builder.build();
+        }
+    }
+
+    public void populateTargets(PluginDefinition plugin) {
+        final Optional<List<UnmodifiableConfig>> targets = config.getOptional("targets");
+        if (targets.isEmpty()) return;
+        for (var conf : targets.get()) {
+            //TODO
+        }
+    }
+
+    private DependencyDefinition buildDep(UnmodifiableConfig conf) {
+        final String id = conf.get("id");
+        final Optional<String> verStr = conf.getOptional("version");
+        // TODO version range, ...
+        return verStr.map(s -> DependencyDefinition.build(id, s)).orElseGet(() -> DependencyDefinition.build(id));
     }
 
 }
